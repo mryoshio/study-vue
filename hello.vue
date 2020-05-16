db3940b0de15439c791a5160664cabb56c937b0f
mryoshio <yoshiokaas@gmail.com>
2020-05-16 11:25:23 +0900

    feat: scoped style

---
+++ hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <p class='message'>msg: {{msg}}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return { msg: 'hello world' }
+  }
+}
+</script>
+
+<style scoped>
+  .message { color: #99cc00; }
+</style>
+
