1773f155762c8138072694d2249cfd78933d8684
mryoshio <yoshiokaas@gmail.com>
2020-05-16 12:59:39 +0900

    feat: double style in sfc

---
+++ cssmodule/double_styles.vue
@@ -0,0 +1,54 @@
+<template>
+  <form novalidate>
+    <p :class='alertValidation'>{{validMessage}}</p>
+    <textarea @input='onInput' :class='textboxValidation'></textarea>
+  </form>
+</template>
+
+<script>
+export default{
+  data() {
+    return {
+      valid: false 
+    }
+  },
+  computed: {
+    validMessage() {
+      return this.valid ? 'OK' : 'NG'
+    },
+    alertValidation() {
+      return this.valid ? this.alert.success : this.alert.error
+    },
+    textboxValidation() {
+      return this.valid ? this.textbox.success : this.textbox.error
+    }
+  },
+  methods: {
+    isRequired(value) {
+      return value.length > 3
+    },
+    onInput(e) {
+      this.valid = this.isRequired(e.target.value)
+    }
+  }
+}
+</script>
+
+<style module='alert'>
+.success {
+  color: green;
+}
+.error {
+  font-weight: bold;
+  color: red;
+}
+</style>
+
+<style module='textbox'>
+.success {
+  color: solid 2px green;
+}
+.error {
+  color: solid 2px red;
+}
+</style>
---
+++ cssmodule/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <p :class="$style.message">
+    こんにちは！
+  </p>
+</template>
+
+<script>
+export default{
+  created() {
+    // eslint-disable-next-line
+    console.log('cssmodules:$style', this.$style)
+  }
+}
+</script>
+
+<style module>
+  .message { color: #42b983; }
+</style>
