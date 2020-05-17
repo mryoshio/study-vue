34470d3bfb2dd315e862d6bd48b837d81caabc4a
mryoshio <yoshiokaas@gmail.com>
2020-05-17 22:39:57 +0900

    chore: gomi

---
+++ vuex/task.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          name: 'buy milk',
+          done: false,
+          labelIds: [1, 2]
+        },
+        {
+          id: 2,
+          name: 'buy  vue.js book',
+          done: true,
+          labelIds: [1, 3]
+        }
+      ],
+      labels: [
+        {
+          id: 1,
+          text: 'grossary'
+        },
+        {
+          id: 2,
+          text: 'food'
+        },
+        {
+          id: 3,
+          text: 'book'
+        }
+      ]
+    }
+  }
+}      
+</script>
