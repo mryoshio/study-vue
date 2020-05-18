4034c31225a753ef8b33103f2215696cef7ddd95
mryoshio <yoshiokaas@gmail.com>
2020-05-18 11:09:01 +0900

    chore: vuex, add & toggle

--- vuex/app.vue
+++ vuex/app.vue
@@ -3,18 +3,42 @@
   <h2>Tasks</h2>
   <ul>
     <li v-for='task in tasks' :key='task.id'>
-      <input type='checkbox' :checked='task.done'>
+      <input type='checkbox' :checked='task.done' @change='toggle(task)'>
       {{task.name}}
     </li>
   </ul>
+  <form @submit.prevent='add'>
+    <input type='text' v-model='newTaskName' placeholder='New Task'>
+  </form>
 </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      newTaskName: ''
+    }
+  },
   computed: {
     tasks() {
       return this.$store.state.tasks
+    }
+  },
+  methods: {
+    add() {
+      // eslint-disable-next-line
+      console.log('add', this.newTaskName)
+
+      this.$store.commit('addTask', {
+        name: this.newTaskName
+      })
+      this.newTaskName = ''
+    },
+    toggle(task) {
+      this.$store.commit('toggleTaskStatus', {
+        id: task.id
+      })
     }
   }
 }
--- vuex/store.js
+++ vuex/store.js
@@ -18,7 +18,26 @@ const store = new Vuex.Store({
         done: true,
         labelIds: [1, 3]
       }
-    ]
+    ],
+    nextTaskId: 3
+  },
+  mutations: {
+    addTask(state, { name }) {
+      state.tasks.push({
+        id: state.nextTaskId,
+        name,
+        done: false
+      })
+      state.nextTaskId++
+    },
+    toggleTaskStatus(state, { id }) {
+      const filtered = state.tasks.filter(task => {
+        return task.id === id
+      })
+      filtered.forEach(task => {
+        task.done = !task.done
+      })
+    }
   }
 })
 
