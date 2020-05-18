1d941eff24b182f57666403e593ac3c7103e5fbd
mryoshio <yoshiokaas@gmail.com>
2020-05-18 14:47:38 +0900

    feat: vuex, component access

---
+++ vuex/from_components/app.vue
@@ -0,0 +1,21 @@
+<template>
+<div id='app'>
+  <p>{{count}}</p>
+  <button @click='incr(1)'>+1</button>
+</div>
+</template>
+
+<script>
+export default {
+  computed: {
+    count() {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    incr(v) {
+      this.$store.commit('increment', { val: v })
+    }
+  }
+}
+</script>
---
+++ vuex/from_components/store.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 1
+  },
+  mutations: {
+    increment(state, { val }) {
+      // eslint-disable-next-line
+      console.log('increment', val)
+
+      this.state.count += val
+    }
+  }
+})
+
+export default store
