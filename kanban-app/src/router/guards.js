76cf6cf5cdc511e849e1f737539c3ac489afa928
mryoshio <yoshiokaas@gmail.com>
2020-05-21 15:05:03 +0900

    fix: typo

--- kanban-app/src/router/guards.js
+++ kanban-app/src/router/guards.js
@@ -1,7 +1,7 @@
 import store from '../store'
 
 export const authorizeToken = (to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
+  if (to.matched.some(record => record.meta.requireAuth)) {
     if (!store.state.auth || !store.state.auth.token) {
       next({ path: '/login' })
     } else {
