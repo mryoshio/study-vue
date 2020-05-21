e7ddf7e4897fb079d16b086e6cc94ff6be10e311
mryoshio <yoshiokaas@gmail.com>
2020-05-21 11:17:32 +0900

    feat: store/action templates

---
+++ kanban-app/src/store/actions.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import * as types from './mutation-types'
+import { Auth, List, Task } from '../api'
+/* eslint-enable no-unused-vars */
+
+export default {
+  login: ({ commit }) => {
+    throw new Error('login action not implemented')
+  }
+  fetchLists: ({ commit }) => {
+    throw new Error('fetchLists action not implemented')
+  }
+  addTask: ({ commit }) => {
+    throw new Error('addTask action not implemented')
+  }
+  updateTask: ({ commit }) => {
+    throw new Error('updateTask action not implemented')
+  }
+  removeTask: ({ commit }) => {
+    throw new Error('removeTask action not implemented')
+  }
+  logout: ({ commit }) => {
+    throw new Error('logout action not implemented')
+  }
+}
